75f451ffa8ebaa6ba6906f6b0a70b57e64e8c09c
kebin20 <ktanzyl@hotmail.com>
2022-11-19 07:43:56 +0900

    Imported project from codepen

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-// Import stylesheets
-import './style.css';
+const email = document.getElementById("email");
+
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Please enter a valid email!");
+    email.reportValidity();
+  } else {
+    email.setCustomValidity("");
+  }
+});
 
